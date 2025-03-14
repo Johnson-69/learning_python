3cc00b18dac84918ee914771f9bec1ffa78c7ce3
Johnson-69 <johnsonsoenyunt@gmail.com>
2025-03-14 15:04:59 +0630

    Updated bicycles.py and Created motorcycles.py

--- list/bicycles.py
+++ list/bicycles.py
@@ -1,2 +1,11 @@
+#Retrieving an element from a list
+
 bicycles = ['trek', 'cannondale', 'redline', 'specialized']
 print(bicycles)
+print(bicycles[0])
+print(bicycles[2])
+print(bicycles[3].title())
+print(bicycles[-2])
+
+message = f"My first bicycle was a {bicycles[0].title()}."
+print(message)
---
+++ list/motorcycles.py
@@ -0,0 +1,6 @@
+# Changing an element in a list
+motorcycles = ['honda', 'yamaha', 'suzuki']
+print(motorcycles)
+
+motorcycles[0] = 'ducati'
+print(motorcycles)
