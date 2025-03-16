ad37e8f95a9f95be9fe393d5816d1c0cc7f5be3a
Johnson-69 <johnsonsoenyunt@gmail.com>
2025-03-17 00:56:19 +0630

    Created files: forLoop.py, rewrite.py

---
+++ tuple/forLoop.py
@@ -0,0 +1,3 @@
+dimensions = (200, 50, 100)
+for dimension in dimensions:
+    print(dimension)
---
+++ tuple/rewrite.py
@@ -0,0 +1,9 @@
+dimensions = (200, 50, 100)
+print("Original dimensions:")
+for dimension in dimensions:
+    print(dimension)
+
+dimensions = (300, 200, 400)
+print("\nNew dimensions:")
+for dimension in dimensions:
+    print(dimension)
